89606d0ec0af106041cbd51fc8a052a576ee8d6b
rodrigoruan16 <rodrigoruan16@hotmail.com>
2025-06-06 02:38:37 -0300

    Mudanças no front-end

---
+++ src/front/scripts/script.js
@@ -0,0 +1,65 @@
+async function cadastrar(e) {
+  e.preventDefault();
+
+  const email = document.getElementById('email').value.trim();
+  const username = document.getElementById('username').value.trim();
+  const password = document.getElementById('password').value;
+
+  const data = {
+    "email": email,
+    "username": username,
+    "password": password
+  }
+
+  const res = await fetch('http://localhost:3000/cadastrar', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(res => res.json())
+  .then(data => {
+    if (data.success) {
+      alert("Usuário cadastrado com sucesso.");
+      window.location.href = './login.html';
+    } else {
+      alert(data.message);
+    }
+  })
+  .catch(err => alert('Erro ao cadastrar.'));
+
+  console.log(res);
+}
+
+async function entrar(e) {
+  e.preventDefault();
+
+  const email = document.getElementById('email').value.trim();
+  const password = document.getElementById('password').value;
+
+  const data = {
+    "email": email,
+    "password": password
+  }
+
+  const res = await fetch('http://localhost:3000/logar', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(res => res.json())
+  .then(data => {
+    if (data.success) {
+      alert("Logado com sucesso.");
+      window.location.href = './homepage.html';
+    } else {
+      alert(data.message);
+    }
+  })
+  .catch(err => alert(err));
+
+  console.log(res);
+}
